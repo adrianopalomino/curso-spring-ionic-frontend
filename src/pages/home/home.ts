b3a4771de9db862bff92f3133e0396755064e840
adriano.dantas <adriano.dantas@ctis.com.br>
2020-09-16 21:15:00 -0300

    Interceptor para incluir token nas requisições

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage, MenuController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
+import { AuthService } from '../../services/auth.service';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  creds: CredenciaisDTO = {
+    email: "",
+    senha: ""
+  };
+
+  constructor(
+    public navCtrl: NavController, 
+    public menu: MenuController,
+    public authService: AuthService) {
 
   }
+
+  ionViewWillEnter() {
+    //desabilita o menu
+    this.menu.swipeEnable(false);
+  }
+  ionViewDidLeave() {
+    //habilita o menu
+    this.menu.swipeEnable(true);
+  }
+
+  login(){
+    this.authService.authenticate(this.creds)
+    .subscribe(response => {
+      this.authService.successfulLogin(response.headers.get('Authorization'));
+      this.navCtrl.setRoot('CategoriasPage');
+     },
+     error => {}
+     );
+  } 
+
 
 }
